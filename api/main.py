b59087595e97c22a2bb5283fde8cf818fe0cea65
Kosuke Hosoda <kosukehosoda@Kosukenonotobukkukonpyuta.local>
2024-10-02 21:54:56 +0900

    Ignore __pycache__ directories and .pyc files

--- api/main.py
+++ api/main.py
@@ -1,5 +1,17 @@
+from typing import Union, List, Any
 from fastapi import FastAPI
 from api.routers import task
+from pydantic import BaseModel
 
 app = FastAPI()
-app.include_router(task.router)
+# app.include_router(task.router)
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tags: List[str] = []
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item) -> Any:
+    return Item
